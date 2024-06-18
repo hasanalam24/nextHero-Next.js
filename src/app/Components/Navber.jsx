4768adf7e1e8033d1de218f53158568d69322241
hasanalam24 <hasanalam365@gmail.com>
2024-06-18 10:35:44 +0600

    navigation redirect page btn link

--- src/app/Components/Navber.jsx
+++ src/app/Components/Navber.jsx
@@ -1,11 +1,11 @@
 "use client"
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navber = () => {
 
     const pathName = usePathname()
-
+    const router = useRouter()
 
     const links = [
         {
@@ -22,6 +22,10 @@ const Navber = () => {
         },
     ]
 
+    const handlerLogin = () => {
+        router.push('/login')
+    }
+
 
     return (
         <nav className="bg-red-500 p-6 text-white flex items-center justify-between">
@@ -33,7 +37,7 @@ const Navber = () => {
                     links.map(link => <Link className={`${pathName === link.path && 'text-cyan-600'}`} key={link.path} href={link.path}>{link.title}</Link>)
                 }
             </ul>
-
+            <button onClick={handlerLogin} className="text-orange-600 bg-white font-semibold p-4">Login</button>
         </nav >
     );
 };
