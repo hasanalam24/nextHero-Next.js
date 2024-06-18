90f8108a4d66e9633f2305d88786974f0cdcc18e
hasanalam24 <hasanalam365@gmail.com>
2024-06-18 10:30:02 +0600

    dynamic pathname select

--- src/app/Components/Navber.jsx
+++ src/app/Components/Navber.jsx
@@ -1,20 +1,40 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navber = () => {
+
+    const pathName = usePathname()
+
+
+    const links = [
+        {
+            title: 'About',
+            path: '/about'
+        },
+        {
+            title: 'Services',
+            path: '/services'
+        },
+        {
+            title: 'Contact',
+            path: '/contact'
+        },
+    ]
+
+
     return (
         <nav className="bg-red-500 p-6 text-white flex items-center justify-between">
             <h5 className="font-bold">Next<span className="text-blue-800">Hero</span></h5>
 
             <ul className="flex items-center justify-center space-x-4">
 
-                <li>
-                    <Link href="about">About</Link>
-                </li>
-                <li>Service</li>
-                <li>Contact</li>
+                {
+                    links.map(link => <Link className={`${pathName === link.path && 'text-cyan-600'}`} key={link.path} href={link.path}>{link.title}</Link>)
+                }
             </ul>
 
-        </nav>
+        </nav >
     );
 };
 
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+            Contact page
+        </div>
+    );
+};
+
+export default page;
